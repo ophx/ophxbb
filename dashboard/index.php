57f95f53a0c42b9ef730126a69f76c6b7ad3a765
ophx <castytastyhappybat@gmail.com>
2023-09-12 11:11:58 -0400

    custom scrollbar

--- dashboard/index.php
+++ dashboard/index.php
@@ -20,6 +20,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/scroll.css">
 </head>
 <body class="bg-[#0f0f0f]" style="font-family: 'Poppins', sans-serif !important;" onload="spin()">
     <!-- Loader -->
--- index.php
+++ index.php
@@ -21,6 +21,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/scroll.css">
     <style>
         .floating { 
             animation-name: floating;
